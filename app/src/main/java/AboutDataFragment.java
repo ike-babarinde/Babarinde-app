7ea9370ce35cc1225cdc46261402edd7257f3d6e
Ikeoluwa <ikeoluwa.babarinde@stu.cu.edu.ng>
2021-03-26 11:24:55 +0100

    Initial commit

---
+++ app/src/main/java/AboutDataFragment.java
@@ -0,0 +1,21 @@
+package com.example.babarindeapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.babarindeapp.R;
+
+public class AboutDataFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fargement_aboutdata1, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/babarindeapp/DataVisualizationFragment.java
@@ -0,0 +1,22 @@
+package com.example.babarindeapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.babarindeapp.R;
+
+public class DataVisualizationFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragement_datavisaulization, container, false);
+    }
+}
+
